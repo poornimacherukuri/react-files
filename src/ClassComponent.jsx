122c4671826c356ef4a5d74cbb8c8e2ff20859a7
poornimacherukuri <poornima2186@gmail.com>
2025-10-08 11:54:24 +0530

    added

--- src/ClassComponent.jsx
+++ src/ClassComponent.jsx
@@ -2,7 +2,7 @@ import React,{Component} from "react";
 class ClassComponent extends Component{
     render(){
         return(
-            <h1> This is Class-Component</h1>
+            <h1>This is a Class-Component</h1>
         )
     }
 }
